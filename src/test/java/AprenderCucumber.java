86350da3ebda2162678fad0de82c6d83021d2c24
Joao Romao <joao.romao@mobicare.com.br>
2021-05-22 20:54:28 -0300

    7. Tudo em portugues

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,20 +1,20 @@
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
-	@Given("^que criei o arquivo corretamente$")
+	@Dado("^que criei o arquivo corretamente$")
 	public void que_criei_o_arquivo_corretamente() throws Throwable {
-		System.out.println("primeiro");
+	    System.out.println("Primeiro");
 	}
 
-	@When("^executa-lo$")
+	@Quando("^executa-lo$")
 	public void executa_lo() throws Throwable {
-		System.out.println("segundo");
+		System.out.println("Segundo");
 	}
 
-	@Then("^a especificação deve finalizar com sucesso$")
+	@Então("^a especificação deve finalizar com sucesso$")
 	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-		System.out.println("terceiro");
+		System.out.println("Terceiro");
 	}
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -1,9 +1,8 @@
-Feature: Aprendendo Cucumber
-	Como um aluno
-	Eu quero aprender a utilizar Cucumber
-	Para que eu possa automatizar critérios de aceitação
+# language: pt
+Funcionalidade: Aprendendo Cucumber
 	
-	Scenario: Deve executar especificação
-		Given que criei o arquivo corretamente
-		When executa-lo
-		Then a especificação deve finalizar com sucesso
+	
+	Cenario: Deve executar especificação
+		Dado que criei o arquivo corretamente
+		Quando executa-lo
+		Então a especificação deve finalizar com sucesso
