a01ba3346d31cb17848a8753a3d3d82bbd396dd4
Joao Romao <joao.romao@mobicare.com.br>
2021-05-22 23:13:48 -0300

    12. Reaproveitando cenarios = regex

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.java.pt.Dado;
@@ -7,34 +11,65 @@ import cucumber.api.java.pt.Quando;
 public class AprenderCucumber {
 
 	@Dado("^que criei o arquivo corretamente$")
-	public void queCrieiOArquivoCorretamente(){
+	public void queCrieiOArquivoCorretamente() {
 		System.out.println("Primeiro");
 	}
 
 	@Quando("^executa-lo$")
-	public void executaLo(){
+	public void executaLo() {
 		System.out.println("Segundo");
 	}
 
 	@Então("^a especificação deve finalizar com sucesso$")
-	public void aEspecificaçãoDeveFinalizarComSucesso(){
+	public void aEspecificaçãoDeveFinalizarComSucesso() {
 		System.out.println("Terceiro");
 	}
 
 	private int contador = 0;
 
 	@Dado("^que o valor do contador é (\\d+)$")
-	public void que_o_valor_do_contador_é(int arg1){
+	public void que_o_valor_do_contador_é(int arg1) {
 		contador = arg1;
 	}
 
 	@Quando("^eu incrementar em (\\d+)$")
-	public void eu_incrementar_em(int arg1){
+	public void eu_incrementar_em(int arg1) {
 		contador += arg1;
 	}
 
 	@Então("^o valor do contador sera (\\d+)$")
-	public void o_valor_do_contador_sera(int arg1){
+	public void o_valor_do_contador_sera(int arg1) {
 		Assert.assertEquals(arg1, contador);
+	}
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void que_a_entrega_é_dia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void a_entrega_atrasar_em_dias(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if (tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		if (tempo.equals("meses")) {
+			cal.add(Calendar.MONDAY, arg1);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void a_entrega_será_efetuada_em(String data) throws Throwable {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
 	}
 }
