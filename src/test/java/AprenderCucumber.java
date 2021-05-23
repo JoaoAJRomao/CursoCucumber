3cca12b73529a459b8c2723693708008bd78e5c6
Joao Romao <joao.romao@mobicare.com.br>
2021-05-22 22:02:12 -0300

    9. CucumberOptions

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -3,18 +3,19 @@ import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
+
 	@Dado("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-	    System.out.println("Primeiro");
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+		System.out.println("Primeiro");
 	}
 
 	@Quando("^executa-lo$")
-	public void executa_lo() throws Throwable {
+	public void executaLo() throws Throwable {
 		System.out.println("Segundo");
 	}
 
 	@Então("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
 		System.out.println("Terceiro");
 	}
 }
